08c007b383bf885e6d27c8eb1fe73901d39301a3
liujiuwu <liujiuwu@gmail.com>
2009-01-14 22:31:11 +0800

    modify

--- app/controllers/my_wares_controller.rb
+++ app/controllers/my_wares_controller.rb
@@ -6,17 +6,16 @@ class MyWaresController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    
   end
   
   def my_wares
     url = URI.parse('http://sip.alisoft.com/sip/rest')
-    sessionid = '063261cda065408e9401d81973e72c10'#Time.now.strftime("%Y%m%d%H%M%S")
+    login_url,json,sip_appkey,sip_appsecret,sessionid = nil,'','20426','144098c0d58411ddbc14a92bef58a353','063261cda065408e9401d81973e72c10'
+
     p = {
-      'sip_appkey' => '20426',
-      'sip_appsecret' => '144098c0d58411ddbc14a92bef58a353',
+      'sip_appkey' => sip_appkey,
+      'sip_appsecret' => sip_appsecret,
       'sip_apiname' => 'taobao.items.onsale.get',
-      #'sip_apiname' => 'taobao.items.get',
       'sip_timestamp' => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
       'sip_sessionid' => sessionid,
       'page_no' => params[:page_no],
@@ -25,18 +24,13 @@ class MyWaresController < ApplicationController
       'fields' => 'iid,title,price,list_time,num,pic_path, delist_time',
       'format' => 'json'
     }
-    p["sip_sign"] = MD5.hexdigest('144098c0d58411ddbc14a92bef58a353' + p.sort.flatten.join).upcase
+    p["sip_sign"] = MD5.hexdigest(sip_appsecret + p.sort.flatten.join).upcase
 
-    redirect_url = nil
     resp  = Net::HTTP.post_form(url, p)
     doc = REXML::Document.new(resp.body)
-    doc.elements.each("error_rsp/url") { |e| redirect_url = e.text }
-    #result = JSON.parse(resp.body)
-    unless redirect_url.nil?
-      redirect_to "#{redirect_url}&sip_redirecturl=http://127.0.0.1:3000/my_wares"
-    else
-      render :text => resp.body
-    end
+    doc.elements.each("error_rsp/url") { |e| login_url = e.text }
+    json = login_url.nil? ?  resp.body : "{login_url:\"#{login_url}&sip_redirecturl=http://127.0.0.1:3000/my_wares\"}"
+    render :text => json
   end
 
 end
